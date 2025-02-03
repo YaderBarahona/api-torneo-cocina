ec18512f11287f6428afe0bb97bca937d4d9ae54
Yader <yaderbarahona180@gmail.com>
2025-02-03 04:23:41 -0600

    first commit

--- src/server.js
+++ src/server.js
@@ -2,14 +2,15 @@ import app from "../src/app.js";
 import { AppDataSource } from "../src/config/database.js";
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "0.0.0.0"; 
 
 AppDataSource.initialize()
   .then(() => {
     console.log("Base de datos conectada con éxito");
 
     app.listen(PORT, () => {
-      console.log(`Servidor corriendo en http://localhost:${PORT}`);
-      console.log(`Documentación corriendo en http://localhost:${PORT}/api-docs`);
+      console.log(`Servidor corriendo en http://${HOST}:${PORT}`);
+      console.log(`Documentación corriendo en http://${HOST}:${PORT}/api-docs`);
 
     });
   })
