22ffe851e9a6bb13a7340437c8aa783236624f8e
Yader <yaderbarahona180@gmail.com>
2025-02-03 04:30:35 -0600

    first commit

--- src/server.js
+++ src/server.js
@@ -8,10 +8,10 @@ AppDataSource.initialize()
   .then(() => {
     console.log("Base de datos conectada con éxito");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, HOST, () => {
       console.log(`Servidor corriendo en http://${HOST}:${PORT}`);
-      console.log(`Documentación corriendo en http://${HOST}:${PORT}/api-docs`);
-
+      console.log(`Documentación disponible en http://${HOST}:${PORT}/api-docs`);
     });
+    
   })
   .catch((error) => console.error("Error al conectar la base de datos:", error));
